e4c758dba683467e4164cdf243affe283e9abaf0
SandyMrtss <bebesubi2@hotmail.com>
2024-01-21 20:31:14 +0100

    Initial commit

---
+++ src/test/java/cat/itacademy/barcelonactiva/martos/sandra/s05/t01/n03/S05T01N03MartosSandraApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.martos.sandra.s05.t01.n03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N03MartosSandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
