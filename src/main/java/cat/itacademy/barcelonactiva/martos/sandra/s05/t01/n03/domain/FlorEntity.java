202d0b9715966a798fe1fd4a8ad72ae4e7a6ce81
SandyMrtss <bebesubi2@hotmail.com>
2024-01-22 19:06:59 +0100

    data model

---
+++ src/main/java/cat/itacademy/barcelonactiva/martos/sandra/s05/t01/n03/domain/FlorEntity.java
@@ -0,0 +1,41 @@
+package cat.itacademy.barcelonactiva.martos.sandra.s05.t01.n03.domain;
+
+
+public class FlorEntity {
+    private Integer pk_FlorID;
+    private String nomFlor;
+    private String paisFlor;
+
+    public FlorEntity() {
+    }
+
+    public FlorEntity(Integer pk_FlorID, String nomFlor, String paisFlor) {
+        this.pk_FlorID = pk_FlorID;
+        this.nomFlor = nomFlor;
+        this.paisFlor = paisFlor;
+    }
+
+    public Integer getPk_FlorID() {
+        return pk_FlorID;
+    }
+
+    public void setPk_FlorID(Integer pk_FlorID) {
+        this.pk_FlorID = pk_FlorID;
+    }
+
+    public String getNomFlor() {
+        return nomFlor;
+    }
+
+    public void setNomFlor(String nomFlor) {
+        this.nomFlor = nomFlor;
+    }
+
+    public String getPaisFlor() {
+        return paisFlor;
+    }
+
+    public void setPaisFlor(String paisFlor) {
+        this.paisFlor = paisFlor;
+    }
+}
